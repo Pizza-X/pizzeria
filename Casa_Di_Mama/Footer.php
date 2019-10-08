c27889fd507c9ad046af9c57c630ff9e647cdd3d
Thierry-Br <56155930+Thierry-Br@users.noreply.github.com>
2019-10-08 15:43:02 +0200

    Add files via upload

---
+++ Casa_Di_Mama/Footer.php
@@ -0,0 +1,64 @@
+
+<html>
+<head>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="keywords" content="footer, address, phone, icons" />
+
+	<title>Footer With Address And Phones</title>
+	
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+    <link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="styling_Footer.css">
+
+</head>
+
+	<body>
+		<footer class="footer-distributed">
+                    <div id="page-container">
+			<div class="footer-left">
+
+				<p class="footer-links">
+					<a href="#">Home</a>
+					·
+					<a href="#">Prijs</a>
+					·
+					<a href="#">Over</a>
+					·
+					<a href="#">Faq</a>
+					·
+					<a href="#">Contact</a>
+				</p>
+
+				<p class="footer-company-name">Pizzeria Casa Di Mama</p>
+			</div>
+
+			<div class="footer-center">
+
+				<div>
+					<i class="fa fa-map-marker"></i>
+					<p><span>Pizza straat 116</span> Groningen, Nederland</p>
+				</div>
+
+				<div>
+					<i class="fa fa-phone"></i>
+					<p>+31 6 12345678</p>
+				</div>
+
+			</div>
+
+			<div class="footer-right">
+
+				<p class="footer-company-about">
+					<span>Over het bedrijf</span>
+					Pizzeria Casa Di Mama is een opkomende Pizzeria.
+				</p>
+
+			</div>
+                    </div>
+		</footer>
+
+	</body>
+
+</html>
