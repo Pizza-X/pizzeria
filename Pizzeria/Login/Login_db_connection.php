2290be36059840d0cf127714579b75ca605bcc44
sanderb16 <56153606+sanderb16@users.noreply.github.com>
2019-10-10 13:56:48 +0200

    Add files via upload

---
+++ Pizzeria/Login/Login_db_connection.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+$host="localhost";
+$user="root";
+$password="";
+$db="pizzeria";
+
+// mysql_connect($host, $user, $password);
+// mysql_select_db($db);
+$con = mysqli_connect($host, $user, $password,$db);
+
+if(isset($_POST['gebruikersnaam'])){
+    
+    $gebruikersnaam=$_POST['gebruikersnaam'];
+    $wachtwoord=$_POST['wachtwoord'];
+    $_SESSION['gebruikersnaam'] = $gebruikersnaam;
+    
+    $sql="select * from klant where Gebruikersnaam='".$gebruikersnaam."'AND Wachtwoord='".$wachtwoord."' limit 1";
+    
+    $result=mysqli_query($con, $sql);
+    
+    if(mysqli_num_rows($result)==1){
+        echo " Welkom " . $_SESSION['gebruikersnaam'];
+        
+    }
+    else{
+        echo " U heeft het verkeerde wachtwoord of gebruikersnaam opgegeven";
+    }      
+}
+?>
+
