55313ef0365ead731952e8ee6404aa1f00a119d5
sanderb16 <56153606+sanderb16@users.noreply.github.com>
2019-10-10 13:55:35 +0200

    Add files via upload

---
+++ pizzeria.sql
@@ -0,0 +1,199 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Gegenereerd op: 10 okt 2019 om 13:54
+-- Serverversie: 10.4.6-MariaDB
+-- PHP-versie: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pizzeria`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `abonnement`
+--
+
+CREATE TABLE `abonnement` (
+  `Abbonr` int(11) NOT NULL,
+  `Klantnr` int(11) NOT NULL,
+  `Dag` datetime NOT NULL,
+  `Datum` date NOT NULL,
+  `ProductID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `abonnement`
+--
+
+INSERT INTO `abonnement` (`Abbonr`, `Klantnr`, `Dag`, `Datum`, `ProductID`) VALUES
+(1, 1, '2019-10-10 17:01:30', '2019-10-10', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `bestelling`
+--
+
+CREATE TABLE `bestelling` (
+  `Bestellingnr` int(11) NOT NULL,
+  `Totaalprijs` int(11) NOT NULL,
+  `Datum` date NOT NULL,
+  `Klantnr` int(11) NOT NULL,
+  `ProductID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `bestelling`
+--
+
+INSERT INTO `bestelling` (`Bestellingnr`, `Totaalprijs`, `Datum`, `Klantnr`, `ProductID`) VALUES
+(3, 10, '2019-10-10', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `klant`
+--
+
+CREATE TABLE `klant` (
+  `Klantnr` int(11) NOT NULL,
+  `Voornaam` text NOT NULL,
+  `Tussenvoegsel` text NOT NULL,
+  `Achternaam` text NOT NULL,
+  `Emailadres` varchar(150) NOT NULL,
+  `Gebruikersnaam` varchar(35) NOT NULL,
+  `Wachtwoord` varchar(28) NOT NULL,
+  `Geslacht` varchar(1) NOT NULL COMMENT 'm/v',
+  `Geboortedatum` date NOT NULL,
+  `Plaats` varchar(25) NOT NULL,
+  `Postcode` varchar(6) NOT NULL,
+  `Straat` varchar(35) NOT NULL,
+  `Huisnummer` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `klant`
+--
+
+INSERT INTO `klant` (`Klantnr`, `Voornaam`, `Tussenvoegsel`, `Achternaam`, `Emailadres`, `Gebruikersnaam`, `Wachtwoord`, `Geslacht`, `Geboortedatum`, `Plaats`, `Postcode`, `Straat`, `Huisnummer`) VALUES
+(1, 'Piet', '', 'Henk', 'pietje123@gmail.com', 'Pietje', 'Henkje', 'M', '2000-12-12', 'Groningen', '9743PH', 'Zernikeplein', 11);
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `product`
+--
+
+CREATE TABLE `product` (
+  `ProductID` int(11) NOT NULL,
+  `Naam` varchar(25) NOT NULL,
+  `Informatie` text NOT NULL,
+  `Prijs` int(11) NOT NULL,
+  `Ingredienten` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `product`
+--
+
+INSERT INTO `product` (`ProductID`, `Naam`, `Informatie`, `Prijs`, `Ingredienten`) VALUES
+(1, 'Pizza Salami', 'Dit is een hele lekkere pizza met salami erop', 5, 0);
+
+--
+-- Indexen voor geëxporteerde tabellen
+--
+
+--
+-- Indexen voor tabel `abonnement`
+--
+ALTER TABLE `abonnement`
+  ADD PRIMARY KEY (`Abbonr`),
+  ADD KEY `Klantnr` (`Klantnr`),
+  ADD KEY `ProductID` (`ProductID`);
+
+--
+-- Indexen voor tabel `bestelling`
+--
+ALTER TABLE `bestelling`
+  ADD PRIMARY KEY (`Bestellingnr`),
+  ADD KEY `Klantnr` (`Klantnr`),
+  ADD KEY `ProductID` (`ProductID`),
+  ADD KEY `Klantnr_2` (`Klantnr`);
+
+--
+-- Indexen voor tabel `klant`
+--
+ALTER TABLE `klant`
+  ADD PRIMARY KEY (`Klantnr`);
+
+--
+-- Indexen voor tabel `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`ProductID`);
+
+--
+-- AUTO_INCREMENT voor geëxporteerde tabellen
+--
+
+--
+-- AUTO_INCREMENT voor een tabel `abonnement`
+--
+ALTER TABLE `abonnement`
+  MODIFY `Abbonr` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT voor een tabel `bestelling`
+--
+ALTER TABLE `bestelling`
+  MODIFY `Bestellingnr` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT voor een tabel `klant`
+--
+ALTER TABLE `klant`
+  MODIFY `Klantnr` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT voor een tabel `product`
+--
+ALTER TABLE `product`
+  MODIFY `ProductID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Beperkingen voor geëxporteerde tabellen
+--
+
+--
+-- Beperkingen voor tabel `abonnement`
+--
+ALTER TABLE `abonnement`
+  ADD CONSTRAINT `abonnement_ibfk_1` FOREIGN KEY (`Klantnr`) REFERENCES `klant` (`Klantnr`),
+  ADD CONSTRAINT `abonnement_ibfk_2` FOREIGN KEY (`ProductID`) REFERENCES `product` (`ProductID`);
+
+--
+-- Beperkingen voor tabel `bestelling`
+--
+ALTER TABLE `bestelling`
+  ADD CONSTRAINT `bestelling_ibfk_1` FOREIGN KEY (`Klantnr`) REFERENCES `klant` (`Klantnr`),
+  ADD CONSTRAINT `bestelling_ibfk_2` FOREIGN KEY (`ProductID`) REFERENCES `product` (`ProductID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
