6194a6f0027765197b502e7aa8f91847d1d09ef9
sanderb16 <56153606+sanderb16@users.noreply.github.com>
2019-10-18 08:59:38 +0200

    Update register.php

--- Casa_Di_Mama/register.php
+++ Casa_Di_Mama/register.php
@@ -7,64 +7,68 @@
   <link rel="stylesheet" type="text/css" href="styling.css">
 </head>
 <body>
+<style>
+.error2 {color: #FF0000;}
+</style>
   <div class="header">
   	<h2>Register</h2>
   </div>
   <form id=form_register method="post" action="register.php">
   	<?php include('error.php'); ?>
 	<div class="input-group">
-		<label>Voornaam</label>
+		<label>Voornaam <span class="error2">*</span> </label>
 			<input type="text" name="voornaam" value="<?php echo $voornaam; ?>">
+			
 	</div>	
 	<div class="input-group">
-		<label>Tussenvoegsel</label>
+		<label>Tussenvoegsel <span class="error2">*</span> </label>
 			<input type="text" name="tussenvoegsel">
 	</div>
 	<div class="input-group">
-		<label>Achternaam</label>
+		<label>Achternaam <span class="error2">*</span> </label>
 			<input type="text" name="achternaam" value="<?php echo $achternaam; ?>">
 	</div>
 	<div class="input-group">	
-		<label>Email</label>
+		<label>Email <span class="error2">*</span> </label>
 			<input type="email" name="email" value="<?php echo $email; ?>">
 	</div>
 	<div class="input-group">
-		<label>Gebruikersnaam</label>
+		<label>Gebruikersnaam <span class="error2">*</span> </label>
 			<input type="text" name="gebruikersnaam" value="<?php echo $gebruikersnaam; ?>">
 	</div>
 	<div class="input-group">
-		<label>Wachtwoord</label>
+		<label>Wachtwoord <span class="error2">*</span> </label>
 			<input type="password" name="password_1">
 	</div>
 	<div class="input-group">
-		<label>Bevestig wachtwoord</label>
+		<label>Bevestig wachtwoord <span class="error2">*</span> </label>
 			<input type="password" name="password_2">
 	</div>
 	
 	
-		<label>Geslacht</label>
+		<label>Geslacht <span class="error2">*</span> </label>
 			<input type="radio" name="geslacht" value="M"> M
                         <input type="radio" name="geslacht" value="V"> V <br><br>
 	
-		<label> Geboortedatum </label>
+		<label> Geboortedatum <span class="error2">*</span> </label>
 		<div class="input-group">
 			 <input type="date" name="geboortedatum" value="<?php echo $geboortedatum; ?>">
 
         </div>
 	<div class="input-group">
-		<label> <br>Plaats </label>
+		<label> <br>Plaats <span class="error2">*</span> </label>
 			<input type="text" name="plaats" value="<?php echo $plaats; ?>">
 	</div>
 	<div class="input-group">
-		<label>Postcode </label>
+		<label>Postcode <span class="error2">*</span> </label>
 			<input type="text" name="postcode" value="<?php echo $postcode; ?>">
 	</div>
 	<div class="input-group">
-		<label>Straat </label>
+		<label>Straat <span class="error2">*</span> </label>
 			<input type="text" name="straat" value="<?php echo $straat; ?>">
 	</div>
 	<div class="input-group">
-		<label>Huisnummer </label>
+		<label>Huisnummer <span class="error2">*</span> </label>
 			<input type="number" name="huisnummer" value="<?php echo $huisnummer; ?>">
 	</div>
 	  
