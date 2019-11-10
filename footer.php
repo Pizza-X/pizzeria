cc049da48aa49e82d694ae94939dcf58f1875ea6
dontbelieveyou <40839340+dontbelieveyou@users.noreply.github.com>
2019-11-10 15:38:11 +0100

    Add files via upload
    Nieuwe footer & css

---
+++ footer.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+
+<link rel="stylesheet" href="styling_Footer.css">
+
+     </head>
+           <body>
+		   
+		   
+<!-- FOOTER --!>
+<footer>
+ <!-- Grid row -->
+   <div class="row">
+<div class="footer-center">
+ <p <div id="footer-company-name">Pizzeria Casa Di Mama </p> 
+</div>	
+</div>
+</div>
+
+ <!-- Grid row -->
+   <div class="row">
+<div class="footer-links">
+				 <p <div id="footer-company-about" 
+					<span>Over het bedrijf: </span>
+					Pizzeria Casa Di Mama is een opkomende Pizzeria.
+				</p> </div>
+			</div>
+			
+			<div>
+				  <i <p> <div id="fa-fa-phone"></i> 
+					<span>Telnr: </span>
+					 +31 6 57218398
+					</p> </div>
+				    </div>
+				
+			
+</footer>
+<!-- // FOOTER -->
+
+
+      </body>
+
+</html>
